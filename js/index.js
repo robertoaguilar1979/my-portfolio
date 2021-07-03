936200f938b44be27001b8d9a282ab7cc606ca93
robertoaguilar1979 <robertoaguilar1979@gmail.com>
2021-07-03 01:33:01 -0700

    working on up scroll button

--- js/index.js
+++ js/index.js
@@ -2,6 +2,18 @@ const openButton = document.querySelector(".nav-open");
 const closeButton = document.querySelector(".nav-close");
 const navigation = document.querySelector(".nav");
 
+const about = document.querySelector(".nav-item2");
+const contact = document.querySelector(".nav-item3");
+
+
+about.addEventListener("click", () => {
+    navigation.classList.toggle("open");
+})
+contact.addEventListener("click", () => {
+    navigation.classList.toggle("open");
+})
+
+
 openButton.addEventListener("click", () => {
     navigation.classList.add("open");
 });
