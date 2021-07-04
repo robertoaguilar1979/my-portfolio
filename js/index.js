0af3191d87a397fba7539313efb9fbd5789e1b12
robertoaguilar1979 <robertoaguilar1979@gmail.com>
2021-07-04 00:50:59 -0700

    upScroll fixed in javascript

--- js/index.js
+++ js/index.js
@@ -5,6 +5,7 @@ const navigation = document.querySelector(".nav");
 const about = document.querySelector(".nav-item2");
 const contact = document.querySelector(".nav-item3");
 
+const upArrow = document.querySelector(".upScroll");
 
 about.addEventListener("click", () => {
     navigation.classList.toggle("open");
@@ -20,4 +21,17 @@ openButton.addEventListener("click", () => {
 
 closeButton.addEventListener("click", () => {
     navigation.classList.remove("open");
+});
+
+upArrow.addEventListener("click", () => {
+    window.scrollTo(0, 0);
+})
+
+window.addEventListener("scroll", e => {
+
+    if (document.body.scrollTop > 700 || document.documentElement.scrollTop > 700) {
+        upArrow.classList.add("visible");
+    } else {
+        upArrow.classList.remove("visible");
+    }
 });
