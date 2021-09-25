e7624e81573d3310636a629de56ba3bc63fd385c
robertoaguilar1979 <robertoaguilar1979@gmail.com>
2021-09-25 14:22:37 -0700

    updated animations to gsap

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+gsap.registerPlugin(ScrollTrigger);
 let tl = gsap.timeline();
 
 tl.from(".logo-link", {
@@ -6,7 +7,7 @@ tl.from(".logo-link", {
     ease: "back",
     delay: 0.4,
 })
-tl.from(".nav-ul", {
+tl.from(".nav-ul, .burger", {
         duration: 4,
         opacity: 0,
         delay: 0.4,
@@ -15,36 +16,75 @@ tl.from(".nav-ul", {
 
     },
     "<")
-tl.from(".fd", {
+tl.from(".fd, .name, .hi", {
         duration: 1,
         opacity: 0,
-        xPercent: 30
+        xPercent: 30,
+        delay: 0.6,
+    },
+    "<"
+
+)
+tl.from(".hero-img", {
+        duration: 1,
+        opacity: 0,
+        xPercent: -30,
+        delay: 0.6,
     },
     "<"
 
 )
 let tl2 = gsap.timeline();
 
-gsap.from("#la", {
-    scrollTrigger: "#la",
+gsap.from("#la, .paragraph-container, .who-h1", {
     xPercent: -40,
     delay: 0.5,
     opacity: 0,
-    duration: 1.25
+    duration: 1.75,
+    ease: "sine",
+
+    scrollTrigger: {
+        trigger: "#la",
+        start: "top 80%",
+        end: "top 50%",
+        // markers: true,
+        scrub: 1,
+    }
+
+
 });
-gsap.from("#about", {
-    scrollTrigger: "#about",
-    yPercent: -40,
+gsap.from(".karenAndMe", {
+    xPercent: 40,
     delay: 0.5,
     opacity: 0,
-    duration: 1.25
+    duration: 1.75,
+    ease: "sine",
+
+    scrollTrigger: {
+        trigger: ".karenAndMe",
+        start: "top 80%",
+        end: "top 50%",
+        // markers: true,
+        scrub: 1,
+    }
+
+
 });
+
 gsap.from(".myWork-images img", {
-    scrollTrigger: ".myWork-images",
+    // scrollTrigger: ".myWork-images",
     yPercent: 50,
     duration: 1.25,
     ease: "back",
     stagger: 0.5,
     delay: 0.5,
-    opacity: 0
+    opacity: 0,
+    scrollTrigger: {
+        trigger: ".myWork-images img",
+        toggleActions: "restart",
+        start: "top 80%",
+        end: "top 40%",
+        // markers: true,
+        scrub: 1.75,
+    }
 });
